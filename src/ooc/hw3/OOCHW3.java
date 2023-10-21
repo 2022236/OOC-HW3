5bb5109f9d27b4f4bdb4b07ba954bc23495740f6
2022236 <2022236@student.cct.ie>
2023-10-21 19:09:46 +0100

    Comments

--- src/ooc/hw3/OOCHW3.java
+++ src/ooc/hw3/OOCHW3.java
@@ -9,23 +9,30 @@ import java.util.List;
 import java.util.Map;
 
 public class OOCHW3 {
-
     /**
-     * 
+     * Student number: 2022236 
+     * I got inspiration from Sam and Ken to do few parts of the code.
+     * Github link https://github.com/2022236/HW3OOC
+     * @param args
      */
     public static void main(String[] args) {
+        // Starting the code with Map, one of the Java Collections Framework. 
+        // Choosing to start with `Map` seemed like a better idea since `Map` allows to put a key and value which would be 
+        // a String to teams and a value representating the list of team members as Sam resquested
         Map<String, List<String>> teams = new HashMap<>();
-
+        // try-catch also sounded a good idea if some error happens to open the file
         try (BufferedReader myBR = new BufferedReader(new FileReader("mock-data.csv"))) {
             String line;
             int teamNum = 1;
+            // A array list to do the members
             List<String> members = new ArrayList<>();
-
+           // A while to read all the lines an right after calling array list to add the lines  
             while ((line = myBR.readLine()) != null) { 
                 members.add(line);
-
+                // If statement to do a limit on the array, 5 persons per time and ++ to keep counting 
                 if (members.size() == 5) {
                     teams.put("Team" + teamNum, new ArrayList<>(members));
+                    // The members.clear() do not let repeat a person and make sure it is just 5  
                     members.clear();
                     teamNum++;
                 }
@@ -33,8 +40,7 @@ public class OOCHW3 {
         } catch (IOException e) {
            System.out.println("File access error!");
         }
-
-        // Print the teams and their members
+        // A for to go and print the teams starting from one getting the `Map` size 
         for (int i = 1; i <= teams.size(); i++) {
             String teamName = "Team" + i;
             System.out.println(teamName + ": " + teams.get(teamName));
